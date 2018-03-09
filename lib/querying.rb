a1f10a0f392ba343bb48bfeaf11fcde0b622436a
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-09 16:52:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,5 +39,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*) AS book_count
-  JOIN characters"
+  FROM character_books 
+  JOIN characters ON character_books.character_id = characters.id 
+  "
 end
