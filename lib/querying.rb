f1e675543f417d576d7db870a35afac2486ad66a
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-09 16:43:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,9 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM "
+  FROM authors
+  JOIN series ON series.author_id = authors.id 
+  JOIN subgenres on series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
