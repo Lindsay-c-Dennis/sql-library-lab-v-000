9470e2d8c76456eff38e1536dac7a380da9e2ad6
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-09 16:45:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,8 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title 
-  FROM "
+  FROM series 
+  JOIN books on "
 end
 
 def select_character_names_and_number_of_books_they_are_in
